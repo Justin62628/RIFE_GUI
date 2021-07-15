6c300ead79122f922e5be7701ef2cc07bc4f006d
Justin Southern Isles <2410377391@qq.com>
2021-07-15 22:10:04 +0800

    SVFI 3.4.1.alpha functional commit 1. 修复一拍三遇到纯色帧序列时的卡顿 2. 修复单一识别模式爆错 3. 优化一拍N去除前的显示机制（展示进度条） 4. 加入安静模式（无软件弹窗） 5. 替换新图标 6. 关闭黑色弹窗

--- quick_release.py
+++ quick_release.py
@@ -2,16 +2,19 @@
 import os
 
 root = r"D:\60-fps-Project\Projects\RIFE GUI"
-ico_path = os.path.join(root, "svfi.ico")
-gui_version = input("SVFI GUI Version: ")
-cli_version = input("SVFI CLI Version: ")
-tag_version = input("SVFI Tag Version: ")
+ico_path = os.path.join(root, "svfi-i.ico")
+gui_version = "3.4.1"
+cli_version = "6.8.1"
+tag_version = gui_version + ".alpha"
+# gui_version = input("SVFI GUI Version: ")
+# cli_version = input("SVFI CLI Version: ")
+# tag_version = input("SVFI Tag Version: ")
+os.system(
+    f'nuitka --standalone --windows-disable-console --mingw64 --show-memory --show-progress --nofollow-imports --plugin-enable=qt-plugins --windows-icon-from-ico="{ico_path}" --windows-product-name="SVFI CLI" --windows-product-version={cli_version} --windows-file-description="SVFI Interpolation CLI" --windows-company-name="Jeanna-SVFI"  --follow-import-to=Utils --output-dir=release .\one_line_shot_args.py')
 os.system(
     f'nuitka --standalone --mingw64 --show-memory --show-progress --nofollow-imports --include-qt-plugins=sensible,styles --plugin-enable=qt-plugins  --include-package=QCandyUi,PyQt5 --windows-icon-from-ico="{ico_path}" --windows-product-name="SVFI" --windows-product-version={gui_version} --windows-file-description="Squirrel Video Frame Interpolation" --windows-company-name="SVFI" --follow-import-to=Utils --output-dir=release --windows-disable-console .\RIFE_GUI_Start.py')
 # debug
 # os.system(f'nuitka --standalone --mingw64 --show-memory --show-progress --nofollow-imports --include-qt-plugins=sensible,styles --plugin-enable=qt-plugins  --include-package=QCandyUi,PyQt5 --windows-icon-from-ico="{ico_path}" --windows-product-name="SVFI" --windows-product-version={gui_version} --windows-file-description="Squirrel Video Frame Interpolation" --windows-company-name="SVFI" --follow-import-to=Utils --output-dir=release .\RIFE_GUI_Start.py')
-os.system(
-    f'nuitka --standalone --windows-disable-console --mingw64 --show-memory --show-progress --nofollow-imports --plugin-enable=qt-plugins --windows-icon-from-ico="{ico_path}" --windows-product-name="SVFI CLI" --windows-product-version={cli_version} --windows-file-description="SVFI Interpolation CLI" --windows-company-name="Jeanna-SVFI"  --follow-import-to=Utils --output-dir=release .\one_line_shot_args.py')
 pack_dir = r"D:\60-fps-Project\Projects\RIFE GUI\release\release_pack"
 if not os.path.exists(pack_dir):
     os.mkdir(pack_dir)
